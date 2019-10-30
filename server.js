74aa5df99ca0bf564691009fedf8a78e000f1273
Modurotolu Olokode <durolawk@gmail.com>
2019-10-30 15:28:32 +0100

    customized user middleware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const helmet = require('helmet');
+const userRouter = require('./users/userRouter')
 
 server.use(cors());
 server.use(helmet());
@@ -9,6 +10,8 @@ server.use(express.json());
 
 //test stuvs
 server.use(logger)
+
+server.use('/api/user', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,10 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
+const User = require('./userDb')
 
 router.post('/', (req, res) => {
-
+    
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -33,15 +34,40 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    User.getById(req.params.id)
+        .then(user => {
+            if(user) {
+                req.user = user;
+                next();
+            } else {
+                res.status(400).json({ message: "invalid user id" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                'something went wrong checking db' : error.message
+            });
+        });
 };
 
 function validateUser(req, res, next) {
-
+    if(!req.body) {
+        res.status(400).json({ message: "missing user data" });
+    } else if(!req.body.name) {
+        res.status(400).json({ message: "missing required name field" });
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
-
+    if(!req.body) {
+        res.status(400).json({ message: "missing post data" });
+    } else if(!req.body.text) {
+        res.status(400).json({ message: "missing required text field" });
+    } else {
+        next();
+    }
 };
 
 module.exports = router;
