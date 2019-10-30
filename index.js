f539cb27406b51807e28f3ec956090d93c5a880e
Modurotolu Olokode <durolawk@gmail.com>
2019-10-30 14:38:26 +0100

    logger implemented

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require('./server');
+
+server.listen(process.env.PORt || 3000, () => {
+    console.log('server is listening on ' + (process.env.PORt || 3000));
+})
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+const cors = require('cors');
+const helmet = require('helmet');
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+//test stuvs
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +17,10 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `request method: ${req.method}, request url: ${req.url}, timestamp: ${new Date().toISOString()}`
+  );
+  next();
 };
 
 module.exports = server;
