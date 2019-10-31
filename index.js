86c3028f94c96da6647912668811bcd2b5ed7bef
Modurotolu Olokode <durolawk@gmail.com>
2019-10-31 14:38:54 +0100

    fixedtypo on index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server');
 
-server.listen(process.env.PORt || 3000, () => {
-    console.log('server is listening on ' + (process.env.PORt || 3000));
+server.listen(process.env.PORT || 3000, () => {
+    console.log('server is listening on ' + (process.env.PORT || 3000));
 })
