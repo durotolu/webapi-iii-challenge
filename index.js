6114122be7754a74fca4ae04e32abea4b405b58f
Modurotolu Olokode <durolawk@gmail.com>
2019-10-31 14:49:37 +0100

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server');
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log('server is listening on ' + (process.env.PORT || 3000));
+server.listen(process.env.PORT, () => {
+    console.log('server is listening on ' + process.env.PORT);
 })
