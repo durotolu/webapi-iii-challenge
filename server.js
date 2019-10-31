5b258ea647e8eedebba57187e9a7642bcda75888
Modurotolu Olokode <durolawk@gmail.com>
2019-10-31 14:17:53 +0100

    added start script and engine to package.json

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+server.use((req, res) => {
+  res.send('An error was encountered')
+})
+
 //custom middleware
 
 function logger(req, res, next) {
